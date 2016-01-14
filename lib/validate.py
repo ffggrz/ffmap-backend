3c22b725cd1a406621b78b48bc0a7d0abd6f25d1
Marcus <sunzi@viacobra.de>
2016-01-14 14:44:59 +0100

    validate node_id

--- lib/validate.py
+++ lib/validate.py
@@ -16,4 +16,13 @@ def validate_nodeinfo(nodeinfo):
         if 'latitude' not in nodeinfo['location'] or 'longitude' not in nodeinfo['location']:
             return False
 
+    if 'node_id' in nodeinfo:
+        # If node_id contains characters where not a digit or simple letters then reject it
+        # This should allow node_id from MAC without ":" and node_id like "gw1"
+        if re.search(r'[^a-zA-Z0-9]', nodeinfo['node_id']):
+            return False
+    else:
+        # Reject if node_id is missing
+        return False
+
     return True
